e93cbf494c8b28440d248ffc360310fb9316e50f
Gaurigoyanka <73536289+Gaurigoyanka@users.noreply.github.com>
2020-11-17 12:37:28 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var car,wall
 var speed,weight
-fuction  ()
+function setup(){
    speed=random(55,90)
    weight=random(400,1500)
    createCanvas(1600,400)
@@ -8,10 +8,9 @@ fuction  ()
    wall=createSprite(1500,600,20,height/2)
    wall.shapeColor=color(80,80,80)
    car.velocityY=speed
-   background("pink")
-
+}  
 function draw(){
-  
+  background("pink")
     if(wall.x-car.x < (car.width+wall.width)/2)
     {
        car.velocityX=0
